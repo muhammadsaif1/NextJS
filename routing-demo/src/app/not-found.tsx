8c9a93bbec3e7b321d2d18c36010ad3fc3876fcf
Saif <saif.shabir12@gmail.com>
2024-12-12 22:05:13 +0500

    routing/not-found-page

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="bg-black">
+      <h2>Page Not found</h2>
+      <p>Could not find requesed resource</p>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+function NotFoung() {
+  return (
+    <div>
+      <h2>Review Not Found</h2>
+    </div>
+  );
+}
+
+export default NotFoung;
