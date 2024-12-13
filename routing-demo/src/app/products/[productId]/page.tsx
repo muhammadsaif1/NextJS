a438a34625f67e317d9d3b7873bf74d2dce651af
Saif <saif.shabir12@gmail.com>
2024-12-13 23:44:17 +0500

    routing-metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-export default function pageDetails({
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function pageDetails({ params }: Props) {
   return (
     <>
       <h1>details {params.productId}</h1>
