02c137cdc951a565545398eb96e0a06b6248c037
Saif <saif.shabir12@gmail.com>
2024-12-14 14:40:30 +0500

    link component navigation

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productID = 100;
   return (
     <>
       <h1>All products are listed below</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="products/1">Product 1</Link>{" "}
+      </h2>
+      <h2>
+        <Link href="products/2">Product 2</Link>{" "}
+      </h2>
+      <h2>
+        <Link href="products/3" replace>
+          Product 3
+        </Link>{" "}
+      </h2>
+      <h2>
+        <Link href={`products/${productID}`}>Product {productID}</Link>{" "}
+      </h2>
+
+      <Link href="/">Home</Link>
     </>
   );
 }
