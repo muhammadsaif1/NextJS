3b407f6286e78cee026252d5831275a89c2aaeed
Saif <saif.shabir12@gmail.com>
2024-12-14 15:03:26 +0500

    navigating programmatically

---
+++ routing-demo/src/app/order-product/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function OrderProduct() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <h1>OrderProduct</h1>
+      <button onClick={handleClick}>place order</button>
+    </div>
+  );
+}
