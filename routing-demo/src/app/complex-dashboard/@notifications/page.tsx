54600968152265cce6f1599e8cd6edd29862aacf
Saif <saif.shabir12@gmail.com>
2024-12-14 16:28:18 +0500

    unmatched routes

--- routing-demo/src/app/complex-dashboard/@notifications/page.tsx
+++ routing-demo/src/app/complex-dashboard/@notifications/page.tsx
@@ -1,6 +1,12 @@
 import Card from "@/components/card";
+import Link from "next/link";
 import React from "react";
 
 export default function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
