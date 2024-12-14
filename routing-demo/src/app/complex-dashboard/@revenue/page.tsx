f97c8110f333412e1c915607057dbfae7853997b
Saif <saif.shabir12@gmail.com>
2024-12-14 16:14:27 +0500

    parallel routing

---
+++ routing-demo/src/app/complex-dashboard/@revenue/page.tsx
@@ -0,0 +1,6 @@
+import Card from "@/components/card";
+import React from "react";
+
+export default function Revenue() {
+  return <Card>Revenue</Card>;
+}
---
+++ routing-demo/src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,6 @@
+import Card from "@/components/card";
+import React from "react";
+
+export default function Users() {
+  return <Card>Users</Card>;
+}
